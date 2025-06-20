fa3e48a36640f7c8f82adc41b383d342385d0701
Shabnam <sbeiraghian@my.bcit.ca>
2025-06-19 23:30:53 -0700

    initial commit

---
+++ server/routes/bookings.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const Booking = require("../models/booking")
+
+
+router.post("/", async (req, res)=>{
+    try{
+        const booking = new Booking(req.body);
+        await booking.save();
+        res.status(201).json(booking);
+
+    } catch (error){
+        res.status(400).json({ error: " Could not create booking" })
+    }
+})
+
+route.get("/", async (req, res) =>{
+    try{
+        const bookings = await Booking.find().sort({ date: -1 });
+        res.json(bookings);
+    } catch (error) {
+        res.status(500).json({error: " Could not fetch booking "})
+    }
+})
