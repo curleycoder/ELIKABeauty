a8ba038d373db256db378dde2723aba1e27edf4c
Shabnam <sbeiraghian@my.bcit.ca>
2025-07-02 11:45:35 -0700

    gallery layout added

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,19 +1,25 @@
+// server/routes/gallery.js
 const express = require("express");
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 
-router.get("/gallery", (req, res)=>{
-    const galleryPath = path.join(__dirname, "../public/gallery");
-    fs.readdir(galleryPath, (err, files)=>{
-        if(err) return res.status(500).json({error: "Unable to read directory"})
-    })
-     const imageUrls = files
-     .filter(file => /\.(jpg|jpeg|png|gif)$/.test(file))
-     .map(file => `/gallery/${file}`)
+router.get("/", (req, res) => {
+  const galleryPath = path.join(__dirname, "../public/gallery");
 
-     res.json({ image :imageUrls })
-})
+  fs.readdir(galleryPath, (err, files) => {
+    if (err) {
+      console.error("Error reading gallery folder:", err);
+      return res.status(500).json({ error: "Unable to read directory" });
+    }
+
+    const imageUrls = files
+      .filter((file) => /\.(jpg|jpeg|png|gif)$/.test(file))
+      .map((file) => `/gallery/${file}`);
+
+    res.json({ images: imageUrls });
+  });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require("dotenv").config();
+const path = require("path"); 
 
 const app = express();
 app.use(cors());
@@ -9,10 +10,13 @@ app.use(express.json());
 
 const bookingRoutes = require("./routes/bookings");
 const serviceRoutes = require("./routes/services")
+const galleryRoutes = require("./routes/gallery")
 
 app.use("/api/bookings", bookingRoutes)
 app.use("/api/services", serviceRoutes)
+app.use("/api/gallery", galleryRoutes)
 
+app.use('/gallery', express.static(path.join(__dirname, 'public' , 'gallery')))
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
