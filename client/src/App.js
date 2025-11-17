d43be83463ba29857d638f88d511b28e89f5fcc1
Shabnam <sbeiraghian@my.bcit.ca>
2025-11-16 23:16:28 -0800

    built

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 
 import React from "react";
@@ -11,6 +10,8 @@ import AboutMe from "./components/AboutMe"
 import Navbar from "./components/Navbar"
 import Gallery from './components/Gallery';
 import {SpeedInsights} from "@vercel/speed-insights/react"
+import Article from "./pages/Article"
+
 
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/booking" element={<Booking />}/>
           <Route path='/gallery' element={<Gallery/>}/>
-          <Route path="/articles/:slug" element={<ArticlePage />} />
+          <Route path="/articles/:slug" element={<Article />} />
           <Route path="/product" element={<Product />}/>
           <Route path="/aboutme" element={<AboutMe />}/>
         </Routes>
