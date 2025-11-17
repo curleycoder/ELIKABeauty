c28f62cbff51655ef53be9eaeebf40c92b5ed192
Shabnam <sbeiraghian@my.bcit.ca>
2025-11-17 11:44:59 -0800

    add vercel analytics

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HelmetProvider>
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>
   </HelmetProvider>
 );
