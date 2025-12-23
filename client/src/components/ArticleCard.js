0dec9495e8aa49a6525ef328c4ce5ecc4000ee7a
Shabnam <sbeiraghian@my.bcit.ca>
2025-12-23 14:18:28 -0800

    border color changed

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -12,7 +12,7 @@ export default function ArticleCard({ article }) {
       to={`/articles/${slug}`}
       className="
         block bg-white rounded-xl overflow-hidden w-full max-w-md
-        border border-pinkcolor mx-auto
+        border border-pinkcolor/30 mx-auto
         hover:-translate-y-1 transition-transform duration-200
       "
     >
