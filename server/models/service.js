e5e436d27237f09e831d2d923dd53f126f6d1d4c
Shabnam Beiraghian <sbeiraghian@my.bcit.ca>
2026-03-13 19:41:07 +0000

    remove old tree

--- server/models/service.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const serviceSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: String, required: true },
-})
-
-module.exports = mongoose.model("Service", serviceSchema)
